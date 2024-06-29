b2e3544e9d8c4d89c134da8d35f16e510d1765b0
keilyily <keilyzqhuang@gmail.com>
2024-06-29 12:41:44 -0700

    Update robot.hpp

--- v1/src/robot.hpp
+++ v1/src/robot.hpp
@@ -4,7 +4,7 @@
 using namespace keejLib;
 
 namespace robot {
-    DriveTrain dt = keejLib::DriveTrain({3,2,3}, {4,5,6});
+    DriveTrain dt = keejLib::DriveTrain({16,18,17}, {15,13,14});
     
     pros::Controller prosController(pros::E_CONTROLLER_MASTER);
     keejLib::Controller cont = keejLib::Controller(prosController);
