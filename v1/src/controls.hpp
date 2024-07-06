71eb372c67a83f4b6ab9354a57ef1fe6063d16a2
8pxl <56054380+8pxl@users.noreply.github.com>
2024-07-06 09:46:23 -0700

    add controls

--- v1/src/controls.hpp
+++ v1/src/controls.hpp
@@ -1,3 +1,5 @@
+#include "keejLib/controller.h"
+#include "keejLib/util.h"
 #include "keejlib/lib.h"
 #include "robot.hpp"
 #include "main.h"
@@ -6,4 +8,15 @@ using namespace robot;
 
 void driver() {
     dt.spinVolts(cont.drive(1, Controller::driveMode::arcade));
+    auto state = cont.getAll(ALLBUTTONS);
+    
+    if (state[R1]) {
+        intake.move(127);
+    }
+    else if (state[R2]) {
+        intake.move(-127);
+    }
+    else {
+        intake.move(0);
+    }
 }
