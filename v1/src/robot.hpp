3c676030e860c0f94f9df9e90cda1bd6d0181a7d
8pxl <56054380+8pxl@users.noreply.github.com>
2024-07-06 09:46:14 -0700

    add ports

--- v1/src/robot.hpp
+++ v1/src/robot.hpp
@@ -1,12 +1,15 @@
+#pragma once
+
 #include "keejLib/lib.h"
 #include "pros/misc.hpp"
-#pragma once
 using namespace keejLib;
 
 namespace robot {
-    DriveTrain dt = keejLib::DriveTrain({16,18,17}, {15,13,14});
+    DriveTrain dt = keejLib::DriveTrain({-16,18,-17}, {15,-13,14});
     // Chassis chass = keejLib::Chassis(dt, {});
     
     pros::Controller prosController(pros::E_CONTROLLER_MASTER);
     keejLib::Controller cont = keejLib::Controller(prosController);
+    
+    pros::Motor intake(20);
 }
