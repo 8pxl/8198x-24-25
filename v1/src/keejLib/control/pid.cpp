b6c1f5c30e4bdbacf81d41aaece6c3a9c7271320
8pxl <56054380+8pxl@users.noreply.github.com>
2024-08-30 11:53:15 -0700

    dt to pid

--- v1/src/keejLib/control/pid.cpp
+++ v1/src/keejLib/control/pid.cpp
@@ -4,10 +4,13 @@ using namespace keejLib;
 PID::PID(PIDConstants constants) : constants(constants), integral(0){};
 
 double PID::out(double error) {
+    double dt = pros::millis() - prevTime;
+    prevTime = pros::millis();
     if(std::fabs(error) < constants.tolerance) integral = 0;
     else if(std::fabs(error) < constants.integralThreshold) integral += error;
     if(integral > constants.maxIntegral) integral = constants.maxIntegral;
-    derivative = error - prevError;
+    derivative = (error - prevError) / dt;
+    integral *= dt;
     prevError = error;
     return(error * constants.kp + integral * constants.ki + derivative * constants.kd + (error * constants.kf));
 }
