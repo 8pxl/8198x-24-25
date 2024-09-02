60c36143cc964f14878401a0db8a2631283d192b
8pxl <56054380+8pxl@users.noreply.github.com>
2024-09-01 22:54:23 -0700

    add chassis and sensors

--- v1/src/robot.hpp
+++ v1/src/robot.hpp
@@ -3,12 +3,13 @@
 #include "keejLib/lib.h"
 // #include "pros/misc.hpp"
 #include "lift.hpp"
+#include "pros/optical.hpp"
 // #include "pros/rotation.hpp"
 using namespace keejLib;
 
 namespace robot {
     DriveTrain dt = keejLib::DriveTrain({-16,18,-17}, {15,-13,14});
-    Chassis chass = keejLib::Chassis(&dt, {});
+    
     
     pros::Controller prosController(pros::E_CONTROLLER_MASTER);
     keejLib::Controller cont = keejLib::Controller(prosController);
@@ -22,6 +23,19 @@ namespace robot {
     pros::ADIDigitalOut intakePiston('D');
     
     pros::Rotation rotationSensor(19);
+    pros::Rotation vertTracker(11);
+    pros::Rotation horizTracker(3);
+    pros::Optical optical(1);
+    
+    pros::Imu imu(5);
+    
+    Chassis chass = keejLib::Chassis(&dt, {
+        .horizWidth = 0,
+        .vertWidth = 0,
+        .trackDia = 4.0,
+        .wheelDia = 2.0,
+        .gearRatio = 0.75,
+    }, &imu, &vertTracker, &horizTracker);
     
     Pis clamp({clampPiston}, false);
     Pis tilt({tiltPiston}, false);
