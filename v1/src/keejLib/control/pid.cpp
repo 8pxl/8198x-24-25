9d4ee115b929b151e1fc4334113c35ee382961fc
8pxl <56054380+8pxl@users.noreply.github.com>
2024-09-07 17:17:34 -0700

    reset integral when crossing target

--- v1/src/keejLib/control/pid.cpp
+++ v1/src/keejLib/control/pid.cpp
@@ -6,8 +6,10 @@ PID::PID(PIDConstants constants) : constants(constants), integral(0){};
 
 double PID::out(double error) {
     prevTime = pros::millis();
+    if(std::fabs(error) < constants.integralThreshold) integral += error;
+    else integral = 0;
     if(std::fabs(error) < constants.tolerance) integral = 0;
-    else if(std::fabs(error) < constants.integralThreshold) integral += error;
+    if (sign(error) != sign(prevError)) integral = 0;
     // if(integral > constants.maxIntegral) integral = constants.maxIntegral;
     integral = std::clamp(integral, -constants.maxIntegral, constants.maxIntegral);
     derivative = (error - prevError);
