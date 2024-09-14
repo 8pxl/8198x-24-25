b2ad5b221a588ee9adbff5e1ab5e730dcf65d9b1
8pxl <56054380+8pxl@users.noreply.github.com>
2024-09-14 02:25:30 -0700

    add turnTo

--- v1/src/keejLib/movement/1dpid.cpp
+++ v1/src/keejLib/movement/1dpid.cpp
@@ -15,7 +15,7 @@ void Chassis::turn(double angle, MotionParams params = {.async = false, .timeout
     Angle targ = Angle(angle, HEADING);
     Exit* timeout = new exit::Timeout(params.timeout);
     // Exit* range = new exit::Range(params.settleRange, params.settleTime);
-    PID cont = PID(this -> angConsts);
+    PID cont = PID(this -> turnConsts);
     double error = targ.error(Angle(imu -> get_rotation(), HEADING));
     while (!params.exit -> exited({.error = fabs(error)}) && !timeout -> exited({})) {
         error = targ.error(Angle(imu -> get_rotation(), HEADING));
@@ -27,4 +27,29 @@ void Chassis::turn(double angle, MotionParams params = {.async = false, .timeout
     moving = false;
 }
 
+void Chassis::turnTo(Pt target, MotionParams params) {
+    moving = true;
+    if (params.async) {
+        params.async = false;
+        pros::Task task([&]() { turnTo(target, params);});
+        pros::delay(10);
+        return;
+    }
+    Angle targ = absoluteAngleToPoint(pose.pos, target);
+    if (params.reverse) targ = targ.reverseDir();
+    Exit* timeout = new exit::Timeout(params.timeout);
+    // Exit* range = new exit::Range(params.settleRange, params.settleTime);
+    PID cont = PID(this -> turnConsts);
+    double error = targ.error(Angle(imu -> get_rotation(), HEADING));
+    while (!params.exit -> exited({.error = fabs(error)}) && !timeout -> exited({})) {
+        targ = absoluteAngleToPoint(pose.pos, target);
+        if (params.reverse) targ = targ.reverseDir();
+        error = targ.error(Angle(imu -> get_rotation(), HEADING));
+        double vel = cont.out(error);
+        this -> dt -> spinVolts(vel, -vel);
+        pros::delay(10);
+    }
+    this -> dt -> spinVolts(0, 0);
+    moving = false;
+}
 }
