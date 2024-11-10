ca24bca015d51e25028380966f56857cd04b70d6
8pxl <56054380+8pxl@users.noreply.github.com>
2024-11-09 20:07:00 -0800

    add controls

--- v2/src/controls.hpp
+++ v2/src/controls.hpp
@@ -1,6 +1,8 @@
+#include "autons/constants.hpp"
 #include "keejLib/lib.h"
 #include "keejLib/util.h"
 #include "robot.hpp"
+#include "autons/autons.hpp"
 
 using namespace robot;
 
@@ -9,16 +11,34 @@ void driver() {
     std::vector<bool> state = cont.getAll(ALLBUTTONS);
     std::vector<bool> released = cont.getReleased();
     
-    if (state[NR1]) intake.move(127);
-    else if (released[R1]) intake.move(0);
+    // if (state[R1]) intakeMotor.move(127);
+    // else if (state[R2]) intakeMotor.move(-127);
+    // else intakeMotor.move(0);
+    // std::cout << released[R1] << std::endl;
+    if (state[R1]) intake.move(127);
+    else if (state[R2]) intake.move(-127);
+    else intake.move(0);
     
-    if (state[NR2]) intake.move(-127);
-    else if (released[NR2]) intake.move(0);
     
     if (state[NL1]) lb.next();
     if (state[NL2]) lb.prev();
-    // else lift.move(0);
     
     if (state[NRIGHT]) lb.score();
     if (state[NY]) clamp.toggle();
+    
+    if (state[NB]) doink.toggle();
+    if (state[NA]) claw.toggle();
+    if (state[NUP]) tsukasa.toggle();
+    if (state[NDOWN]) {
+        // test();
+        // rPos();
+        chass.setTurn(_90);
+        auto x = chass.measureOffsets(10);
+        std::cout << x.first << " " << x.second << std::endl;
+    }
+    if (state[NLEFT]) {
+        auto p = chass.getPose();
+        prosController.print(0, 0, "%.2f, %.2f, %.2f", p.pos.x, p.pos.y, p.heading.heading());
+    }
 }
+
