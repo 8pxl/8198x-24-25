64dfbcf15dfab8197de289d929d3fd45320728e6
8pxl <56054380+8pxl@users.noreply.github.com>
2025-02-03 08:40:50 -0800

    red elims tuning

--- v3/src/autons/neg/relims.hpp
+++ v3/src/autons/neg/relims.hpp
@@ -13,6 +13,7 @@ void relimsNeg() {
       chass.setLin(_lin);
       chass.setMTP(_chassLin, _chassAng);
 
+
       //grab mogo and score alliance stake;
       lb.next();
       chass.driveAngle(-930, 0, {.async = true, .exit = new Range(180, 10), .slew = 5});
@@ -27,7 +28,7 @@ void relimsNeg() {
 
       //score alliance ring
       // chass.driveAngle(800, neg(10), {});
-      Pt allianceStake = {-26.3, 18.9};
+      Pt allianceStake = {-26.1, 18.9};
       // chass.moveWithin(allianceStake, 14.7, {});
       chass.mtpoint(allianceStake, {.drift = 12, .within = 14.7});
       lb.next();
@@ -45,39 +46,46 @@ void relimsNeg() {
       chass.driveAngle(800, 87, {.exit = new Range(40, 10)});
 
       //intake second ring
-      chass.driveAngle(-900, 110, {.exit = new Range(100, 10)});
-      chass.driveAngle(-900, 140, {.exit = new Range(30, 10)});
-      Pt ring2 = {20, -28};
-      chass.mtpoint(ring2, {.vMin = 50, .within = 4});
-      chass.driveAngle(-1000, 140, {.vMin = 20, .exit = new Range(30, 10)});
-      // ringAngle = chass.turnTo(ring2, {.exit = new Range(13, 10)});
-      // chass.driveAngle(1000, ringAngle, {.vMin = 50});
+      chass.driveAngle(-900, 110, {.vMin = 70, .exit = new Range(200, 10)});
+      chass.driveAngle(-900, 135, {.vMin = 30, .exit = new Range(150, 10)});
+      Pt ring2 = {22.1, -28};
+      ringAngle = chass.turnTo(ring2, {.exit = new Range(5, 10)});
+      chass.driveAngle(900, ringAngle, {.vMin = 50});
+      chass.driveAngle(-500, 70, {.vMin = 50, .exit = new Range(90, 10)});
 
       //go to corner
       // chass.mtpoint({-74, 2.5}, {.exit = new Range(6, 10), .drift = 7, .within = 6});
-      Pt corner1 = {47, 24};
-      chass.turnTo(corner1, {.timeout = 200});
-      chass.mtpoint(corner1, {.timeout = 790, .exit = new Range(1, 10), .drift = 10, .within = 9});
+      //was 27.4
+      Pt corner1 = {27.8, 7};
+      // chass.turnTo(corner1, {.t  imeout = 200});
+      chass.mtpoint(corner1, {.timeout = 1200, .exit = new Range(1, 10), .drift = 7});
       intake.move(127);
-      for (int i = 0; i<4; i++)  {
-            // chass.mtpoint(corner1, {.timeout = 790, .exit = new Range(1, 10), .drift = 10, .within = 13});
-            chass.driveAngle(-680, 45, {.timeout = 600, .slew = 2});
-            chass.driveAngle(800, 45, {.timeout = 600, .slew = 2});
-      }
-
+      chass.setTurn(_5);
+      chass.turnTo({41, 17}, {});
+      chass.setTurn(_90);
+      // chass.turn(45, {});=
+      intake.setJamProtection(false);
+      // chass.driveLin(600, 40, {});
+chass.driveAngle(470, 45, {.timeout = 600, .slew = 3});
+            chass.driveLin(600, -25, {});
+            intake.setJamProtection(true);
+      tsukasa.toggle();
+      // pros::delay(50);
+      intake.move(127);
+      chass.driveAngle(800, 45, {.timeout = 600, .slew = 2.4});
+      tsukasa.toggle();
+      chass.driveAngle(-600, 45, {.timeout = 600, .slew = 2});
+      
       //go to third ring
-      chass.driveAngle(-600, neg(60), {.vMin = 40, .exit = new Range(100, 10)});
+      chass.driveAngle(-600, 45, {.vMin = 40, .exit = new Range(100, 10)});
       Pt ring3 = {-29, -3};
+      tsukasa.toggle();
+      chass.turn(neg(90), {.timeout = 300});
       chass.mtpoint(ring3, {.drift = 10, .within = 5});
 
-      Pt corner2 = {-107, 24};
       intake.move(127);
-      chass.mtpoint(corner2, {.timeout = 790, .exit = new Range(1, 10), .drift = 10, .within = 9});
-      chass.driveAngle(-300, neg(45), {.timeout = 600, .slew = 2});
-      chass.turn(90, {.timeout = 250});
-      chass.turn(140, {.async = true, .timeout = 340});
-      pros::delay(300);
-      // chass.turnTo(ring5, {.async = true, .timeout = 600});
-      clamp.toggle();
-      chass.waitUntilSettled();
+      tsukasa.toggle();
+
+      Pt corner2 = {-102, 24};
+      chass.mtpoint(corner2, {.timeout = 1200, .exit = new Range(1, 10), .drift = 10, .within = 9});
 }
