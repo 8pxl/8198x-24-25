226d62d0126471595e7212f948227f0c88e090b4
8pxl <56054380+8pxl@users.noreply.github.com>
2025-02-06 02:23:56 -0800

    add all autos

--- v3/src/autons/autons.hpp
+++ v3/src/autons/autons.hpp
@@ -7,31 +7,28 @@
 #include "skills.hpp"
 #include "neg/rawp.hpp"
 #include "neg/bawp.hpp"
+#include "neg/belims.hpp"
 
 
 using namespace robot;
 using namespace exit;
 
 void test() {
-    // Pt wallStake = {38, -65};
-
-    // chass.turnTo(wallPrime1, {.timeout = 600, .exit = new Range(2, 30)});
-    // chass.measureOffsets(5);
-    // chass.setLin(_lin);
-    // chass.setAng(_ang);
-    // chass.driveAngle(9, 0, {.timeout = 1500, .vMin=0, .exit = new Range(1, 100), .reverse=false});
-    // lb.setAutoControl(false);
-
-pros::delay(700);
-    // intake.setSorting(false);
-pros::delay(700);
-intake.move(0); 
-pros::delay(700);
-intake.move(127);
-lb.next();
-// intake.setSorting(false);
-pros::delay(700);
-intake.move(127);
+    chass.setTurn(_90);
+    chass.setAng(_ang);
+    chass.setMTP(_chassLin, _chassAng);
+    chass.setLin(_lin);
+    Pt blueAS = {-27.2415365831112, -138.53376928669638};
+    // Pt blueAS = {-29, -135};
+    chass.turnTo(blueAS, {.timeout  = 600, .exit = new Range(2, 100)});
+    Pose pose = chass.getPose();
+    std::cout << "x: " << pose.pos.x << " y: " << pose.pos.y << " heading: " << pose.heading.heading() << std::endl;
+    //11.9 < x
+    // chass.moveWithin(blueAS, 12.7, {.timeout = 1500, .vMin=0, .exit = new Range(5, 10)});
+    // chass.moveWithin(blueAS, 17.537751502925833, {.timeout = 1500, .vMin=0, .exit = new Range(5, 10)});
+      chass.mtpoint( blueAS, {.drift = 8, .within = 14.7});
+    pose = chass.getPose();
+std::cout << "x: " << pose.pos.x << " y: " << pose.pos.y << " heading: " << pose.heading.heading() << std::endl;
 
 }
 
@@ -46,4 +43,4 @@ void triangulatePoint() {
     prosController.print(0, 0, "%f, %f", point.x, point.y);
 }
 
-keejLib::Autons autons = {{skills, relimsNeg, bAwp, rAwp, qbPos, qrPos, test}, {"skills", "red elims neg", "blue awp", "red awp", "bPos", "rPos", "test"}};
+keejLib::Autons autons = {{skills,  belimsNeg, relimsNeg, bAwp, rAwp, qbPos, qrPos, test}, {"skills", "blue elims neg", "red elims neg", "blue awp", "red awp", "bPos", "rPos", "test"}};
