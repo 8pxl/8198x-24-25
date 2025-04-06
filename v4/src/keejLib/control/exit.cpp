05a059f603eb5113ada4627b2f68532bd284cd8a
8pxl <56054380+8pxl@users.noreply.github.com>
2025-04-06 12:30:50 -0700

    update perp exit condition

--- v4/src/keejLib/control/exit.cpp
+++ v4/src/keejLib/control/exit.cpp
@@ -18,15 +18,20 @@ bool exit::Range::exited(exitParams params) {
     return (sw.elapsed() > timeout);
 }
 
-exit::Perp::Perp(Pt target, Angle targetHeading): target(target), targetHeading(targetHeading) {
-}
+exit::Perp::Perp(Pt target, Angle targetHeading): target(target), targetHeading(targetHeading) {}
 
-bool exit::Perp::exited(exitParams params) {
-    int newSide = computeSide(
+exit::Perp::Perp(Pt target): target(target) {}
+
+int exit::Perp::computeSide(exitParams params) {
+    return keejLib::computeSide(
         params.pose.pos, 
         target.value_or(params.target),
         targetHeading.value_or(params.targetHeading)
     );
+}
+
+bool exit::Perp::exited(exitParams params) {
+    int newSide = computeSide(params);
     if (side.has_value() && newSide != side.value()) {
         return true;
     }
